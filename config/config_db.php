4e1f8dc495c0c67b7811f3169f37a02117a39bb5
Winder Ojeda <winder.alejandro@gmail.com>
2017-03-18 15:15:10 -0400

    composer para deploy

--- config/config_db.php
+++ config/config_db.php
@@ -1,13 +1,23 @@
 <?php
  class DB extends DBmysql {
                 
- var $dbhost = 'localhost';
+ /*var $dbhost = 'localhost';
                 
  var $dbuser 	= 'root';
                 
  var $dbpassword= '';
                 
- var $dbdefault	= 'glpi';
+ var $dbdefault	= 'glpi';*/
+
+ var $dbhost = 'us-cdbr-iron-east-03.cleardb.net';
+                
+ var $dbuser 	= 'b5534b2794fe7d';
+                
+ var $dbpassword= '050f7d16';
+                
+ var $dbdefault	= 'heroku_c6139e9c1ad4d09';
                 
  } 
+
+ //mysql://b5534b2794fe7d:050f7d16@us-cdbr-iron-east-03.cleardb.net/heroku_c6139e9c1ad4d09?reconnect=true
 ?>
